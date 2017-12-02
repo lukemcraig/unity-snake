0aff152b0ce5793cf52889e78995a660d653879c
Luke M Craig <lukemcduffiecraig@gmail.com>
2017-12-02 15:13:37 -0500

    changed the coloring into an update loop

---
+++ Assets/Scripts/Systems/AgeSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgeSystem : EgoSystem<
+EgoConstraint<AgeComponent>
+> {
+    public override void Start()
+    {
+        EgoEvents<TickEvent>.AddHandler( Handle );
+    }
+
+    void Handle( TickEvent e )
+    {
+        constraint.ForEachGameObject((egoComponent, age) =>
+        {
+           age.age++;
+        });
+    }
+
+}
+
---
+++ Assets/Scripts/Systems/SnakeMaterialSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMaterialSystem : EgoSystem<
+EgoConstraint<SnakePartComponent,AgeComponent, MeshRenderer>
+>{
+
+    public override void Update(){    	
+        constraint.ForEachGameObject((egoComponent, snakePart, age, renderer) =>
+        	{
+        		
+        		if (age.age == 0)
+        		{
+        			renderer.material = snakePart.newMaterial;
+        			
+        		}
+        		else if (age.age > 0){
+        			renderer.material = snakePart.normalMaterial;
+        			
+        		}
+        		if (snakePart.isPregnant)
+        		{
+        			renderer.material = snakePart.pregnantMaterial;        			
+        		}
+        	});
+    }
+    
+}
